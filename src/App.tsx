e73d0ca8ce742b0889d85f13be7d0efe2723c1a1
ashikmahmud <ashikmamud187@gmail.com>
2022-07-07 22:34:23 +0600

    complete almost project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import "./App.css";
-import Users from "./Components/Users";
+import ViewContainer from "./Components/ViewContainer";
 
 function App() {
   return (
     <div className="App ">
       <div className="container">
         <h2 className="text-center">UniFarm Toggle View With TypeScript.</h2>
-        <Users
-          name="Ashik Mahmud"
-          virtues={["Honest", "Punctual", "Perfect", "Deal"]}
-        />
+
+        <ViewContainer />
       </div>
     </div>
   );
